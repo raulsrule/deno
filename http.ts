b82b7bf0ba5a8e3d49eb52e5ce7304794c683d66
Rahul Kurup <rkurup@chegg.com>
2020-11-17 23:13:37 +0530

    README added

--- http.ts
+++ http.ts
@@ -8,8 +8,17 @@
 import { serve } from "https://deno.land/std@0.78.0/http/server.ts";
 
 const server = serve({ port: 9001 });
+const testHeader = "deno-test-header";
+
+console.log("Waiting for incoming requests...");
+
+// curl localhost:9001
+// curl -i -H "deno-test-header: request-from-curl" localhost:9001
 
 // top-level await, deferring the need for any `async` IIFE
 for await (const req of server) {
+  console.log(
+    `got (${testHeader}) value as ${req.headers.get(testHeader || "none")}`,
+  );
   req.respond({ body: "DENO" });
 }
